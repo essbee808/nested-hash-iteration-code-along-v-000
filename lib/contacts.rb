f2816a03933aeefbaca3768e1f53f50c439ea2c2
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-27 03:41:16 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,9 +18,9 @@ def remove_strawberry(contacts)
   contacts.each do |name|
     if name == "Jon Snow"
       name.each do |key, value|
-        new_array
+        new_array = []
         if key == favorite_ice_cream_flavors
-          
+          new_array << ["strawberry", "cookie dough", "mint chip"].pop
       end 
       end
 end
